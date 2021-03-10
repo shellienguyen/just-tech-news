a1fef861b76aa18d5da005f8cc134efaab95f1b5
sidonie <shelliednnguyen@gmail.com>
2021-03-09 23:34:25 -0800

    hashed password

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const { Model, DataTypes } = require( 'sequelize' );
 const sequelize = require( '../config/connection' );
+const bcrypt = require( 'bcrypt' );
 
 // Create our User model
 class User extends Model {}
@@ -46,6 +47,25 @@ User.init(
             len: [ 4 ]
          }
       }
+   },
+   {
+      hooks: {
+         // set up beforeCreate lifecycle "hook" functionality
+         async beforeCreate( newUserData ) {
+           newUserData.password = await bcrypt.hash( newUserData.password, 10 );
+           return newUserData;
+         },
+         // set up beforeUpdate lifecycle "hook" functionality
+         async beforeUpdate( updatedUserData ) {
+           updatedUserData.password = await bcrypt.hash( updatedUserData.password, 10 );
+           return updatedUserData;
+         }
+      },
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'user'
    },
    {
       // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
