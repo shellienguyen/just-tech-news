362b8c4a6dff6baa317fd3474581ca52c20b7676
sidonie <shelliednnguyen@gmail.com>
2021-03-16 13:12:18 -0700

    authentication with session

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,40 +1,53 @@
 const router = require( 'express' ).Router();
-const sequelize = require('../config/connection');
-const { Post, User, Comment } = require('../models');
+const sequelize = require( '../config/connection' );
+const { Post, User, Comment } = require( '../models' );
 
-router.get('/', (req, res) => {
+router.get( '/', ( req, res ) => {
+   console.log( req.session );
+
    Post.findAll({
       attributes: [
          'id',
          'post_url',
          'title',
          'created_at',
-         [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+         [ sequelize.literal( '(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)' ), 'vote_count' ]
       ],
       include: [
          {
             model: Comment,
-            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            attributes: [ 'id', 'comment_text', 'post_id', 'user_id', 'created_at' ],
             include: {
                model: User,
-               attributes: ['username']
+               attributes: [ 'username' ]
             }
          },
          {
             model: User,
-            attributes: ['username']
+            attributes: [ 'username' ]
          }
       ]
    })
-   .then(dbPostData => {
+   .then( dbPostData => {
       const posts = dbPostData.map( post => post.get({ plain: true }));
       // pass a single post object into the homepage template
       res.render( 'homepage', { posts } );
    })   
-   .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
+   .catch( err => {
+      console.log( err );
+      res.status( 500 ).json( err );
    });
- });
+});
+
+
+router.get( '/login', ( req, res ) => {
+   if ( req.session.loggedIn ) {
+      res.redirect( '/' );
+      return;
+   };
+
+   res.render( 'login' );
+});
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -9,11 +9,25 @@ const hbs = exphbs.create({});
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Connect to the back-end and store the session
+const session = require( 'express-session' );
+const SequelizeStore = require( 'connect-session-sequelize' )( session.Store );
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
 app.use( express.json());
 app.use( express.urlencoded({ extended: true }));
 app.use( express.static( path.join( __dirname, 'public' )));
 app.engine( 'handlebars', hbs.engine );
 app.set( 'view engine', 'handlebars' );
+app.use( session( sess ));
 
 // turn on routes
 app.use( routes );
