81a92677d70622a8cc30f0cb9b8893b725e1c6f7
sidonie <shelliednnguyen@gmail.com>
2021-03-17 16:39:44 -0700

    format date urls plural singular

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require( 'express' );
 const routes = require( './controllers' );
 const sequelize = require( './config/connection' );
 const path = require( 'path' );
+const helpers = require('./utils/helpers');
 
 const exphbs = require( 'express-handlebars' );
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 const app = express();
 const PORT = process.env.PORT || 3001;
